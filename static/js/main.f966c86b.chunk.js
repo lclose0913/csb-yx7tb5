2fa2a704ed4495cc3896347da3fa241b2314ef96
Code Sandbox <hello@codesandbox.io>
2022-07-13 01:14:53 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.f966c86b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact=this.webpackJsonpreact||[]).push([[0],{4:function(t,e){},5:function(t,e,c){"use strict";c.r(e);var n=c(0),r=c(3),o=c(4),s=c.n(o),a=c(1),i=document.getElementById("root");Object(r.createRoot)(i).render(Object(a.jsx)(n.StrictMode,{children:Object(a.jsx)(s.a,{})}))}},[[5,1,2]]]);
+//# sourceMappingURL=main.f966c86b.chunk.js.map
